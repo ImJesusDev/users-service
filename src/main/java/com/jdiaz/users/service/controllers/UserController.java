6f101ea02d6cbcbaa8e771bdab6608ad6cc1811f
Jesus Diaz <jesus740@gmail.com>
2020-07-26 18:18:09 -0500

    included validation to register users

--- src/main/java/com/jdiaz/users/service/controllers/UserController.java
+++ src/main/java/com/jdiaz/users/service/controllers/UserController.java
@@ -1,13 +1,19 @@
 package com.jdiaz.users.service.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,18 +33,30 @@ public class UserController {
 	public ResponseEntity<?> getUsers() {
 		return ResponseEntity.ok(userService.findAll());
 	}
-	
+
 	@PutMapping("/users/{id}")
-	public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable Long id) {
+	public ResponseEntity<?> updateUser(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id) {
+		if (result.hasErrors()) {
+			return validate(result);
+		}
 		user.setId(id);
 		User updatedUser = userService.save(user);
 		return new ResponseEntity<User>(updatedUser, HttpStatus.CREATED);
 	}
 
+	@PostMapping("/register-user")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+		if (result.hasErrors()) {
+			return validate(result);
+		}
+		User newUser = userService.save(user);
+		return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+	}
+
 	@GetMapping("/users/{id}")
 	public ResponseEntity<?> getUser(@PathVariable Long id) {
 		Optional<User> optionalUser = userService.findById(id);
-		if(optionalUser.isPresent()) {
+		if (optionalUser.isPresent()) {
 			return ResponseEntity.ok(optionalUser.get());
 		} else {
 			return ResponseEntity.notFound().build();
@@ -48,11 +66,20 @@ public class UserController {
 	@GetMapping("/users/search-username")
 	public ResponseEntity<?> searchUsername(@RequestParam String username) {
 		Optional<User> optionalUser = userService.findByUsername(username);
-		if(optionalUser.isPresent()) {
+		if (optionalUser.isPresent()) {
 			return ResponseEntity.ok(optionalUser.get());
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+
+	}
+
+	private ResponseEntity<?> validate(BindingResult result) {
+		Map<String, Object> errors = new HashMap<>();
+		result.getFieldErrors().forEach(error -> {
+			errors.put(error.getField(), "The field " + error.getField() + " " + error.getDefaultMessage());
+		});
+		return ResponseEntity.badRequest().body(errors);
 
 	}
 
